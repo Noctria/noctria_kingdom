548286279687b41a1558bd328ccd8a630a9f146e
Noctoria <masayuki.ohfuku@gmail.com>
2025-06-17 03:31:07 +0900

    Update test_windows_gpu_dag.py

--- airflow_docker/dags/test_windows_gpu_dag.py
+++ airflow_docker/dags/test_windows_gpu_dag.py
@@ -3,14 +3,13 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id='test_windows_gpu_dag',
-    start_date=datetime(2025, 6, 17),
+    dag_id="test_tensorflow_gpu_from_windows",
+    start_date=datetime(2025, 6, 1),
     schedule_interval=None,
     catchup=False,
-    tags=["test", "windows", "gpu"],
 ) as dag:
 
-    run_windows_script = BashOperator(
-        task_id='run_gpu_test_on_windows',
-        bash_command='powershell.exe -Command "python \\"E:\\\\test_gpu_windows.py\\""',
+    test_gpu = BashOperator(
+        task_id="run_tensorflow_gpu_test",
+        bash_command=r"E:\venv_windows_gpu\Scripts\python.exe E:\test_gpu_tf.py"
     )
