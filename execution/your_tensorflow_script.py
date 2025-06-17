f4927208d40f8855c03c207b29d7eeab2a8b9382
Noctoria <masayuki.ohfuku@gmail.com>
2025-06-18 02:06:46 +0900

    Update your_tensorflow_script.py

--- execution/your_tensorflow_script.py
+++ execution/your_tensorflow_script.py
@@ -1,6 +1,4 @@
-# E:\noctria-kingdom-main\execution\your_tensorflow_script.py
-
 import tensorflow as tf
 
-print("✅ TensorFlow バージョン:", tf.__version__)
-print("🧠 GPU 利用可能:", tf.config.list_physical_devices('GPU'))
+print("TensorFlow バージョン:", tf.__version__)
+print("GPU 利用可能:", tf.config.list_physical_devices('GPU'))
