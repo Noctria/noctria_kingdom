ae40268bc58b8a9320d6ed8fb28b40e4cbc9e09c
Noctoria <masayuki.ohfuku@gmail.com>
2025-06-29 05:56:46 +0900

    Update veritas_pdca_dag.py

--- airflow_docker/dags/veritas_pdca_dag.py
+++ airflow_docker/dags/veritas_pdca_dag.py
@@ -26,17 +26,17 @@ with dag:
 
     generate_task = BashOperator(
         task_id="generate_strategy",
-        bash_command="python3 /noctria_kingdom/veritas/generate_strategy_file.py"
+        bash_command="python3 /noctria_kingdom/airflow_docker/scripts/generate_strategy_file.py"
     )
 
     evaluate_task = BashOperator(
         task_id="evaluate_strategies",
-        bash_command="python3 /noctria_kingdom/veritas/evaluate_generated_strategies.py market_data.csv"
+        bash_command="python3 /noctria_kingdom/airflow_docker/scripts/evaluate_generated_strategies.py /noctria_kingdom/airflow_docker/data/market_data.csv"
     )
 
     push_task = BashOperator(
         task_id="push_adopted_strategies",
-        bash_command="python3 /noctria_kingdom/veritas/github_push.py"
+        bash_command="python3 /noctria_kingdom/airflow_docker/scripts/github_push.py"
     )
 
     generate_task >> evaluate_task >> push_task
