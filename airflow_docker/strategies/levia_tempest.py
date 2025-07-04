79b8351a16b155ad27698931ea13028e7bfcaf6d
Noctoria <masayuki.ohfuku@gmail.com>
2025-07-05 08:42:58 +0900

    Update levia_tempest.py

--- airflow_docker/strategies/levia_tempest.py
+++ airflow_docker/strategies/levia_tempest.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from data.market_data_fetcher import MarketDataFetcher
-from core.risk_management import RiskManagement
+from core.risk_manager import RiskManager
 from core.logger import setup_logger  # 👑 ロガー追加
 
 class LeviaTempest:
