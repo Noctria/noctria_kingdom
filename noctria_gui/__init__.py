ba52f28d55b6e3391bc1aed37c45e27cbc07668e
Noctoria <masayuki.ohfuku@gmail.com>
2025-07-13 04:47:30 +0900

    Update __init__.py

--- noctria_gui/__init__.py
+++ noctria_gui/__init__.py
@@ -1,1 +1,14 @@
+# noctria_gui/routes/__init__.py
+
+import noctria_gui.routes.act_history
+import noctria_gui.routes.act_history_detail
+import noctria_gui.routes.dashboard
+# その他のルートモジュールもインポート
+
+routers = [
+    noctria_gui.routes.act_history.router,
+    noctria_gui.routes.act_history_detail.router,
+    noctria_gui.routes.dashboard.router,
+    # その他のルータを追加
+]
 
