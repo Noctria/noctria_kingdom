0e80b2e2542f92060b276d7bc6ae080449a8f1ad
Noctoria <masayuki.ohfuku@gmail.com>
2025-07-16 03:45:12 +0900

    Update statistics_ranking.py

--- noctria_gui/routes/statistics_ranking.py
+++ noctria_gui/routes/statistics_ranking.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
-from core.path_config import NOCTRIA_GUI_TEMPLATES_DIR, ACT_LOG_DIR
+from src.core.path_config import NOCTRIA_GUI_TEMPLATES_DIR, ACT_LOG_DIR
 
 from collections import defaultdict
 from datetime import datetime
