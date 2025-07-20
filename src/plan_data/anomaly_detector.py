a446337bfc205738da5da16979c6ccde317d5028
Noctoria <masayuki.ohfuku@gmail.com>
2025-07-21 05:16:01 +0900

    Update anomaly_detector.py

--- src/plan_data/anomaly_detector.py
+++ src/plan_data/anomaly_detector.py
@@ -1,1 +1,9 @@
+# src/plan_data/anomaly_detector.py
+def detect_drawdown_anomalies(eval_results, dd_threshold=20):
+    """最大DDが異常値の戦略を検出"""
+    return [r for r in eval_results if r.get("max_drawdown", 0) > dd_threshold]
+
+def detect_low_win_rate(eval_results, rate_threshold=40):
+    """勝率が著しく低い戦略"""
+    return [r for r in eval_results if r.get("win_rate", 100) < rate_threshold]
 
