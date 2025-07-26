33f41b9a0136c8404e4c0ba34bafb27435d186a4
Noctoria <masayuki.ohfuku@gmail.com>
2025-07-27 02:23:01 +0900

    Update __init__.py

--- noctria_gui/routes/statistics/__init__.py
+++ noctria_gui/routes/statistics/__init__.py
@@ -2,11 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-from .statistics_dashboard import router as dashboard_router
-from .statistics_detail import router as detail_router
-# 他サブルーターがあればここでimport
+from .strategy_compare import router as strategy_compare_router
 
-router.include_router(dashboard_router, prefix="/dashboard")
-router.include_router(detail_router, prefix="/detail")
-# 他サブルーターも同様にinclude_routerで統合
-
+router.include_router(strategy_compare_router, prefix="/strategy_compare")
