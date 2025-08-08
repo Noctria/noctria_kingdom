09e660a86eb05464e55164891a534d677fedbb30
Noctoria <masayuki.ohfuku@gmail.com>
2025-08-09 01:57:25 +0900

    Create standard_feature_schema.py

---
+++ src/plan_data/standard_feature_schema.py
@@ -0,0 +1,12 @@
+# src/plan_data/standard_feature_schema.py
+STANDARD_FEATURE_ORDER = [
+    "USDJPY_Close",
+    "USDJPY_Volatility_5d",
+    "SP500_Close",
+    "VIX_Close",
+    "News_Count",
+    "CPIAUCSL_Value",
+    "FEDFUNDS_Value",
+    "UNRATE_Value",
+    # ...必要に応じて追加...
+]
