e320246d888ff965ae14c9e5a9a6854273f01ac3
Noctoria <masayuki.ohfuku@gmail.com>
2025-08-15 01:34:39 +0900

    Update Aurus_Singularis.py

--- src/strategies/veritas_generated/Aurus_Singularis.py
+++ src/strategies/veritas_generated/Aurus_Singularis.py
@@ -1,1 +1,7 @@
+# src/strategies/veritas_generated/Aurus_Singularis.py
+class AurusSingularis:
+    def __init__(self, *args, **kwargs):
+        pass
 
+    def propose(self, *args, **kwargs):
+        return {}
